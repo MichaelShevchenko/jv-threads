256a20284fb386c449486cf5ab6beb55bbbc8f74
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-11-06 06:28:20 +0200

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import org.apache.log4j.Logger;
+
+public class Application {
+    private static final Logger logger = Logger.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+}
